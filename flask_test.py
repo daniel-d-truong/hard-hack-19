12d711e9445c8cfd8d29465924156b19b99a0d7d
Daniel Truong <ddtruong06@gmail.com>
2019-01-19 14:23:24 -0800

    set up google cloud vision for local files

--- flask_test.py
+++ flask_test.py
@@ -3,6 +3,7 @@
 # Sources of Help: https://www.youtube.com/watch?v=QnDWIZuWYW0&m=19
 
 from flask import Flask, render_template
+import io
 app = Flask(__name__) #__name__ just means the name of module in python
 
 updates = [
@@ -20,6 +21,38 @@ updates = [
   },
 
 ]
+
+def detect_faces(uri):
+    """Detects faces in an images"""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    # image = vision.types.Image()
+    # image.source.image_uri = uri
+
+    with io.open(uri, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.types.Image(content=content)
+
+    response = client.face_detection(image=image)
+    faces = response.face_annotations
+
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+
+    print ('Faces:')
+    for face in faces:
+        print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+        print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+        print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in face.bounding_poly.vertices])
+
+        print('face bounds: {}'.format(','.join(vertices)))
+
+testPath = "images/canada-head.jpg"
+detect_faces(testPath)
 
 @app.route("/") #pathway of url
 @app.route("/home")
